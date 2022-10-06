42708e1a4e814625fc51d8251b2e3b0c33a020ac
Filipe Marins <filipemarins123@gmail.com>
2022-10-06 10:30:31 -0300

    refactor: oembed and url preview

--- apps/meteor/client/views/room/MessageList/components/UrlPreview/OEmbedHtmlPreview.tsx
+++ apps/meteor/client/views/room/MessageList/components/UrlPreview/OEmbedHtmlPreview.tsx
@@ -1,13 +1,13 @@
 import { Box } from '@rocket.chat/fuselage';
 import React, { ReactElement } from 'react';
 
-import OEmbedCollapseable from './OEmbedCollapseable';
+import OEmbedCollapsible from './OEmbedCollapsible';
 import type { PreviewMetadata } from './PreviewList';
 
 const OEmbedHtmlPreview = ({ html, ...props }: PreviewMetadata): ReactElement => (
-	<OEmbedCollapseable {...props}>
+	<OEmbedCollapsible {...props}>
 		{html && <Box fontScale='p2' withRichContent dangerouslySetInnerHTML={{ __html: html }} />}
-	</OEmbedCollapseable>
+	</OEmbedCollapsible>
 );
 
 export default OEmbedHtmlPreview;
